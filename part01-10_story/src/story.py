2d4e0c1524beab64f97ebedb071cbc4a1c0eb6fd
tarik-yilmaz <tarik.yilmaz.email@gmail.com>
2026-05-12 17:32:38 +0200

    added solution part01-10

---
+++ part01-10_story/src/story.py
@@ -0,0 +1,7 @@
+# Write your solution here
+name = input("Please type in a name: ")
+year = input("Please type in a year: ")
+
+print(name + " is a valiant knight, born in the year " + year + ". One morning " + name + " woke up to an")
+print("awful racket: a dragon was approaching the village. Only " + name + " could save the")
+print("village's residents.")
