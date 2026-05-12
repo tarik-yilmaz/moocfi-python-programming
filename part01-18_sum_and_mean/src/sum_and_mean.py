b2a95637b634ae5f0d448f1a559c00c93cca308b
tarik-yilmaz <tarik.yilmaz.email@gmail.com>
2026-05-12 19:34:33 +0200

    added solution part01-18

---
+++ part01-18_sum_and_mean/src/sum_and_mean.py
@@ -0,0 +1,8 @@
+# Write your solution here
+sum = 0
+sum += int(input("Number 1: "))
+sum += int(input("Number 2: "))
+sum += int(input("Number 3: "))
+sum += int(input("Number 4: "))
+
+print(f"The sum of the numbers is {sum} and the mean is {sum / 4}")
