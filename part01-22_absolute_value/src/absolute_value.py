d316ae184c4523ada46b58656993458ab8446491
tarik-yilmaz <tarik.yilmaz.email@gmail.com>
2026-05-12 20:00:20 +0200

    added solution part01-22

---
+++ part01-22_absolute_value/src/absolute_value.py
@@ -0,0 +1,8 @@
+# Write your solution here
+
+number = int(input("Please type in a number: "))
+
+if number < 0:
+    print(f"The absolute value of this number is {number * (-1)}")
+
+print(f"The absolute value of this number is {number}")
