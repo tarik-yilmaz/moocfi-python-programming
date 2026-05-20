f126d12eeebf1bc4f56373d607f79797464019e5
tarik-yilmaz <tarik.yilmaz.email@gmail.com>
2026-05-20 22:49:43 +0200

    added solution part03-28

---
+++ part03-28_seven_brothers/src/seven_brothers.py
@@ -0,0 +1,8 @@
+# Write your solution here
+
+def seven_brothers():
+    print("Aapo\nEero\nJuhani\nLauri\nSimeoni\nTimo\nTuomas")
+
+# You can test your function by calling it within the following block
+if __name__ == "__main__":
+    seven_brothers()
