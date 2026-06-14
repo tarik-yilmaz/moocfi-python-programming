997b0fea7a6d82e20e5a3a627c84e5e05f6346ed
tarik-yilmaz <tarik.yilmaz.email@gmail.com>
2026-06-14 17:17:47 +0200

    added solution part05-18

---
+++ part05-18_phone_book_v2/src/phone_book_v2.py
@@ -0,0 +1,51 @@
+# Write your solution here
+def usage():
+    command = int(input("command (1 search, 2 add, 3 quit): "))
+
+    if  type(command) != int or command < 1 or command > 3:
+        return
+    
+    return command
+
+def search(book: dict):
+    name = input("name: ").strip()
+            
+    if name in book:
+        for num in book[name]:
+            print(num)
+    else:
+        print("no number")
+
+
+def add(book: dict):
+    name = input("name: ").strip()
+    number = input("number: ").strip()
+
+    if type(name) != str and type(number) != str:
+        return
+
+    if name not in book:
+        book[name] = []
+        
+    book[name].append(number)
+    print("ok!")
+        
+
+def main():
+    book = {}
+
+    while True:
+        command = usage()
+
+        if command == 1:
+            search(book)
+            
+        elif command == 2:
+            add(book)
+            
+        elif command == 3:
+            print("quitting...")
+            return
+
+
+main()
