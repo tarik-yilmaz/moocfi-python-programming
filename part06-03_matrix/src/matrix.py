33dadbb227465a01409af992b4530acf63b128b8
tarik-yilmaz <tarik.yilmaz.email@gmail.com>
2026-06-18 16:05:02 +0200

    update exercise

--- part06-03_matrix/src/matrix.py
+++ part06-03_matrix/src/matrix.py
@@ -1,48 +1,58 @@
 # write your solution here
 
+def read_matrix():
+    matrix_list = []
+
+    with open("src/matrix.txt") as new_file:
+        for line in new_file:
+            line = line.replace("\n", "")
+            number_list = line.split(",")
+
+            row_list = []
+
+            for num in number_list:
+                row_list.append(int(num))
+
+            matrix_list.append(row_list)
+
+    return matrix_list       
+
 def matrix_sum() -> int:
     sum_all = 0
 
-    with open("src/matrix.txt") as new_file:
-        for line in new_file:
-            line = line.replace("\n", "")
-            number_list = line.split(",")
-            
-            for num in number_list:
-                sum_all += int(num)
-            
+    matrix = read_matrix()
+
+    for row in matrix:
+        for num in row:
+            sum_all += num
+
     return sum_all
 
-
 def matrix_max() -> int:
-    max_number = 0
 
-    with open("src/matrix.txt") as new_file:
-        for line in new_file:
-            line = line.replace("\n", "")
-            number_list = line.split(",")
+    matrix = read_matrix()
+    max_number = matrix[0][0]
 
-            for num in number_list:
-                if int(num) > max_number:
-                    max_number = int(num)
-    
+    for row in matrix:
+        for num in row:
+            if num > max_number:
+                max_number = num
+
     return max_number
 
 def row_sums() -> list:
-    sum_of_row = 0
     list_of_row_sums = []
 
-    with open("src/matrix.txt") as new_file:
-        for line in new_file:
-            line = line.replace("\n", "")
-            row_list = line.split(",")
+    matrix = read_matrix()
 
-            for num in row_list:
-                sum_of_row += int(num)
+    for row in matrix:
+        sum_of_row = 0
+
+        for num in row:
+            sum_of_row += num
             
-            list_of_row_sums.append(sum_of_row)
-            sum_of_row = 0
-
+        list_of_row_sums.append(sum_of_row)
+    
     return list_of_row_sums
 
 if __name__ == "__main__":
