86ed8c067bf4f8e5d2f375b662fa9432acbd4c9a
tarik-yilmaz <tarik.yilmaz.email@gmail.com>
2026-08-01 18:18:05 +0200

    added solution part08-10

---
+++ part08-10_decreasing_counter/src/decreasing_counter.py
@@ -0,0 +1,29 @@
+# Tee ratkaisusi tähän:
+class DecreasingCounter:
+
+    def __init__(self, initial_value: int):
+        self.value = initial_value
+        self.reset_value = initial_value
+    def print_value(self):
+        print("value:", self.value)
+
+    def decrease(self):
+        if self.value > 0:
+            self.value -= 1
+
+    def set_to_zero(self):
+        self.value = 0
+
+    def reset_original_value(self):
+        self.value = self.reset_value
+
+
+if __name__ == "__main__":
+    counter = DecreasingCounter(55)
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.decrease()
+    counter.print_value()
+    counter.reset_original_value()
+    counter.print_value()
